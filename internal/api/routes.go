89cc005ae3d8733247a670a0a0e100173ca1e204
Auromagma <maingame2281@gmail.com>
2026-06-10 17:36:41 +0300

    options to everyone

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -23,29 +23,29 @@ func CreateAndRunRoutes() {
     })
   
 	userController := &api_controllers.UserController{repository.GetUserRepository()}
-  apartmentController := &api_controllers.ApartmentController{repository.GetApartmentRepository()}
-  bookingController := &api_controllers.BookingController{repository.GetBookingRepository()}
-  // reviewController := &api_controllers.UserController{repository.GetUserRepository()}
+    apartmentController := &api_controllers.ApartmentController{repository.GetApartmentRepository()}
+    bookingController := &api_controllers.BookingController{repository.GetBookingRepository()}
+    // reviewController := &api_controllers.UserController{repository.GetUserRepository()}
 	
-  // ========== ПУБЛИЧНЫЕ МАРШРУТЫ (без токена) ==========
-  r.HandleFunc("/auth/sign-up", userController.SignUp).Methods("POST")
-	r.HandleFunc("/auth/sign-in", userController.SignIn).Methods("POST")
-	r.HandleFunc("/apartments/{id}", apartmentController.GetApartment).Methods("GET")
-	r.HandleFunc("/apartments", apartmentController.GetAllApartments).Methods("POST")
+    // ========== ПУБЛИЧНЫЕ МАРШРУТЫ (без токена) ==========
+    r.HandleFunc("/auth/sign-up", userController.SignUp).Methods("POST", "OPTIONS")
+	r.HandleFunc("/auth/sign-in", userController.SignIn).Methods("POST", "OPTIONS")
+	r.HandleFunc("/apartments/{id}", apartmentController.GetApartment).Methods("GET", "OPTIONS")
+	r.HandleFunc("/apartments", apartmentController.GetAllApartments).Methods("POST", "OPTIONS")
 	
 	// reviewController := &api_controllers.UserController{repository.GetUserRepository()}
 	// r.HandleFunc("/review/{id}", userController.GetUser).Methods("GET")
 	
-	r.HandleFunc("/bookings/{id}", bookingController.GetBooking).Methods("GET")
-  r.HandleFunc("/users/{id}", userController.GetUser).Methods("GET")
-  r.HandleFunc("/apartments/{id}", apartmentController.GetApartment).Methods("GET")
-  r.HandleFunc("/apartments", apartmentController.GetAllApartments).Methods("POST")
+	r.HandleFunc("/bookings/{id}", bookingController.GetBooking).Methods("GET", "OPTIONS")
+    r.HandleFunc("/users/{id}", userController.GetUser).Methods("GET", "OPTIONS")
+    r.HandleFunc("/apartments/{id}", apartmentController.GetApartment).Methods("GET", "OPTIONS")
+    r.HandleFunc("/apartments", apartmentController.GetAllApartments).Methods("POST", "OTIONS")
 
 	// r.HandleFunc("/review/{id}", userController.GetUser).Methods("GET")
-  // ========== ЗАЩИЩЕННЫЙ МАРШРУТ (с проверкой токена) ==========
-  // Оборачиваем GetUser в AuthMiddleware
-  //r.HandleFunc("/account/apartments", apartmentController.GetAllApartments).Methods("POST")
-  //r.HandleFunc("/booking", bookingController.GetAllBookings).Methods("POST")
+    // ========== ЗАЩИЩЕННЫЙ МАРШРУТ (с проверкой токена) ==========
+    // Оборачиваем GetUser в AuthMiddleware
+    //r.HandleFunc("/account/apartments", apartmentController.GetAllApartments).Methods("POST")
+    //r.HandleFunc("/booking", bookingController.GetAllBookings).Methods("POST")
 
 	port := config.GetSingletonConfig().ServerPort
 	log.Printf("Server starting on port %s", port)
