5ad04555bf174453a71c2a16a2b957e188152eff
AlisaToster <obidjanowa37@gmail.com>
2026-06-21 12:45:07 +0300

    Update userController.go

--- internal/api/controllers/userController.go
+++ internal/api/controllers/userController.go
@@ -101,8 +101,7 @@ func (uc *UserController) SignUp(res http.ResponseWriter, req *http.Request) {
 	}
 
 	go func() {
-		confirmURL := fmt.Sprintf("http://localhost:8080/api/auth/confirm-email?token=%s", token)
-		//confirmURL := fmt.Sprintf("https://team3.verstack.ru/auth/confirm-email?token=%s", token)
+		confirmURL := fmt.Sprintf("https://team3.verstack.ru/auth/confirm-email?token=%s", token)
 
 		data := struct {
 			Name       string
